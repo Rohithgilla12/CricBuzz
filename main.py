7363b1e28139cba0637f27a0c829a9324c29888f
Rohithgilla <gillarohith1@gmail.com>
2017-09-29 12:24:52 +0530

    Added Continuous flow of commentary on the terminal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 url="http://synd.cricbuzz.com/j2me/1.0/livematches.xml"
 r=requests.get(url)
 soup=BeautifulSoup(r.content,'html.parser')
@@ -12,5 +13,29 @@ for i in temp:
     i=i.split('"')
     datapath.append(i[1])
     series_names.append(i[13])
-print datapath
-print series_names
+for i in range(len(datapath)):
+    print str(i+1)+")"+series_names[i]
+ip=input("Enter the match number")
+datapath=datapath[ip-1]
+series_names=series_names[ip-1]
+com_url=datapath+"commentary.xml"
+while(True):
+    time.sleep(15)
+    r=requests.get(com_url)
+    soup=BeautifulSoup(r.content,'html.parser')
+    temp=soup.find('c')
+    temp=str(temp)
+    temp=temp.replace('<c><![CDATA[',"")
+    temp=temp.replace("]]></c>","")
+    print temp
+# print datapath,series_names
+
+
+
+
+
+
+
+
+# print datapath
+# print series_names
