92da62a71068ccef339116d7944ba3395694ae1a
Rohithgilla <gillarohith1@gmail.com>
2017-10-01 18:26:37 +0530

    Added Score i.e runs/wickets format

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ for i in temp:
     series_names.append(i[13])
 for i in range(len(datapath)):
     print str(i+1)+")"+series_names[i]
-ip=input("Enter the match number")
+ip=input("Enter the match number: ")
 datapath=datapath[ip-1]
 series_names=series_names[ip-1]
 com_url=datapath+"commentary.xml"
@@ -29,16 +29,21 @@ while(True):
     print "____"*20
     print temp
     temp=str(soup.find_all('mscr'))
+    runs=temp.split('r="')[3]
+    runs=runs.split('"')[0]
+    wickets=temp.split('wkts="')[1]
+    wickets=wickets.split('"')[0]
     overs=temp.split('ovrs')
     overs=overs[1]
     overs=overs.split('"')[1]
-    print "Overs :"+str(overs)
     batsman=temp.split('btsmn')
     bat1=batsman[1].split('sname="')[1].split('"')[0]
     bat2=batsman[3].split('sname="')[1].split('"')[0]
     r1=batsman[1].split('r="')[1].split('"')[0]
     r2=batsman[3].split('r="')[1].split('"')[0]
-    print "____"*20
+    # print "____"*20
+    print "Score :"+runs + "/"+wickets
+    print "Overs :"+str(overs)
     print bat1+" :"+r1," "+bat2+" :"+r2
     time.sleep(15)
 
